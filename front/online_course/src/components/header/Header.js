4e194bdbdd2f0a33fac9beef94c852e8b3fbcf4b
GooshTest2 <gautier.fenaux@protonmail.com>
2023-05-18 20:55:05 +0200

    ajout animation header

--- front/online_course/src/components/header/Header.js
+++ front/online_course/src/components/header/Header.js
@@ -1,79 +1,61 @@
 import React, { useState, useEffect } from "react";
 import "./header.css";
-import styled from "styled-components";
+// import styled from "styled-components";
 import trompette from "../../assets/trompette.png";
 
 const Header = () => {
+  const pathd =
+    "M0,224L80,213.3C160,203,320,181,480,192C640,203,800,245,960,250.7C1120,256,1280,224,1360,208L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z";
+
+  const finalPathd =
+    "M0,234L80,243.3C160,253,320,273,480,262.7C640,252,800,212,960,207.3C1120,202,1280,232,1360,240L1440,248L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z";
 
   const [scroll, setScroll] = useState(false);
+  const [scrollPos, setScrollPos] = useState(0);
+  const [animationPath, setAnimationPath] = useState(pathd);
+
+  const handleScroll = () => {
+    const currentPosition = window.scrollY;
+    setScrollPos(currentPosition);
+    setScroll(currentPosition > scrollPos);
+    console.log(scrollPos);
+    setAnimationPath(currentPosition > scrollPos ? finalPathd : pathd);
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.pageYOffset > 0) {
-        setScroll(true);
-      } else {
-        setScroll(false);
-      }
-    };
-
     window.addEventListener("scroll", handleScroll);
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []);
+  }, [scrollPos]);
 
   return (
-    <StyledHeader className={`header flex section_padding ${scroll ? "scrolled" : ""}`}>
-      <div className="presentation-container flex-column">
-        <h2>Apprendre la musique en ligne</h2>
-        <p>Apprenez l'instrument que vous avez toujours voulu savoir jouer sans bouger de chez vous !</p>
-        <button></button>
+    <header className="header">
+      <div className="flex section_padding">
+        <div className="presentation-container flex-column">
+          <h2>Apprendre la musique en ligne</h2>
+          <p>
+            Apprenez l'instrument que vous avez toujours voulu savoir jouer sans
+            bouger de chez vous !
+          </p>
+          <button></button>
+        </div>
+        <div className="img-container">
+          <img src={trompette} alt="" />
+        </div>
       </div>
-      <div className="img-container">
-        <img src={trompette} alt="" />
-      </div>
-    </StyledHeader>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          className={`path-to-animate ${scroll ? "animated" : ""}`}
+          fill="#fff"
+          fillOpacity="1"
+          d={animationPath}
+          style={{ transition: "d 1.8s ease" }}
+          transform={`translate(0, ${scrollPos * 0.2}px)`}
+        ></path>
+      </svg>
+    </header>
   );
 };
 
-const StyledHeader = styled.header`
-  /* Styles for the default header */
-  background-color: #77FDAF;
-  padding: 20px;
-
-  &.scrolled {
-    /* Styles for the header when scrolled */
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    transform: scaleY(0.9); /* Scale down the height */
-    transition: transform 0.3s ease; /* Add a transition effect */
-    animation: 2.2s BendBottomLine;
-
-    @keyframes BendBottomLine {
-      from {
-        border-bottom-left-radius: 0px 0px;
-        border-bottom-left-radius: 0px 0px;
-      }
-      to {
-        border-bottom-right-radius: 180px 180px;
-        border-bottom-left-radius: 180px 180px;
-      }
-    }
-  }
-`;
-
-//   return (
-//     <header className="header flex section_padding">
-//       <div className="presentation-container flex-column">
-//         <h2>Apprendre la musique en ligne</h2>
-//         <p>Apprenez l'instrument que vous avez toujours voulu savoir jouer sans bouger de chez vous !</p>
-//         <button></button>
-//       </div>
-//       <div className="img-container">
-//         <img src={trompette} alt="" />
-//       </div>
-//     </header>
-//   );
-// };
-
 export default Header;
-
